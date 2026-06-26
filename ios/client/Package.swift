1cceeb4ab1b99c966b4e601b68f1aab75f752bad
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-06-27 02:35:02 +0800

    Create Package.swift

---
+++ ios/client/Package.swift
@@ -0,0 +1,33 @@
+import PackageDescription
+
+let package = Package(name: "PDClient",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13),
+    ],
+    products: [
+        .library(name: "PDClient", targets: ["PDClient"]),
+    ],
+    dependencies: [
+
+        .package(url: "https://github.com/ProtonMail/protoncore_ios.git", exact: "37.0.1"),
+        .package(url: "https://github.com/ProtonMail/apple-fusion.git", exact: "2.1.5"),
+        .package(url: "https://github.com/getsentry/sentry-cocoa.git", exact: "9.8.0"),
+        .package(url: "https://github.com/Unleash/unleash-proxy-client-swift.git", exact: "2.4.0"),
+        .package(url: "https://github.com/AliSoftware/OHHTTPStubs", exact: "9.1.0"),
+    ],
+    targets: [
+        .target(
+            name: "PDClient",
+            dependencies: [
+                .product(name: "ProtonCoreEnvironment", package: "protoncore_ios"),
+                .product(name: "ProtonCoreNetworking", package: "protoncore_ios"),
+                .product(name: "ProtonCoreServices", package: "protoncore_ios"),
+                .product(name: "ProtonCoreUtilities", package: "protoncore_ios"),
+                .product(name: "Sentry", package: "sentry-cocoa"),
+                .product(name: "UnleashProxyClientSwift", package: "unleash-proxy-client-swift"),
+            ],
+            path: "PDClient"
+        ),
+    ]
+)
